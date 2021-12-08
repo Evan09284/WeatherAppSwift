63142af4db7e93a318963bbb137386a9599a08ee
John2064 <annaopanasenko7@gmail.com>
2021-12-08 13:22:22 +0300

    Update Version

---
+++ SwiftWeather/Forecast - Copy.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Jake Lin on 8/26/15.
+//  Copyright © 2015 Jake Lin. All rights reserved.
+//
+
+import Foundation
+
+struct Forecast {
+  let time: String
+  let iconText: String
+  let temperature: String
+}
